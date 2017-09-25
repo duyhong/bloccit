1fd6eada0ba97c0ab7cc3b8635b8faf5da7c06cf
duyhong <duyhong@berkeley.edu>
2017-09-24 20:07:06 -0700

    bloccit_checkpoint11_assignment: Completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
-  root 'welcome#index'
+  get 'welcome/contact'
 
+  root 'welcome#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
