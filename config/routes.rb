b2350f1807daf5d99d6ffe7cd8e7ccac6e863abb
duyhong <duyhong@berkeley.edu>
2017-10-16 11:13:21 -0700

    Revert "bloccit_checkpoint24_assignment: completed"
    This reverts commit dab7ef4c3c9b1c57b9e79f855c7341bb0be15296.

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
-  post 'users/confirm' => 'users#confirm'
-
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
