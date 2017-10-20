803dcf66ad8fbd78578160f53d75d267bafb3a5e
duyhong <duyhong@berkeley.edu>
2017-10-20 11:43:29 -0700

    bloccit_checkpoint28: completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018204522) do
+ActiveRecord::Schema.define(version: 20171019233301) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20171018204522) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
