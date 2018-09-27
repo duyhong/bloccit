2581dc3f794280c95d486ac8a71904899dc6d68a
duyhong <duyhong@berkeley.edu>
2018-09-27 13:09:45 -0700

    changed config.action_mailer.perform_deliveries = false in config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "bloccit_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # not ready to set up a mailer yet
+  config.action_mailer.perform_deliveries = false
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
